7e38367eab383037f7c5bc552045caaf96f2e71c
Taufan Mahaputra <taufanmhptr@gmail.com>
2018-05-13 03:58:20 +0700

    powerful_words

--- src/controllers/pw_words_controllers.js
+++ src/controllers/pw_words_controllers.js
@@ -1,10 +1,11 @@
 /*jshint node:true*/
 var express   = require('express'),
     words     = require('../models/powerful_words'),
+    data     = require('../data/dummy_review_companies.json'),
     router    = express.Router();
 
 router.get('/:company_name', function(req, res) {
-    words.find({company: req.params.company_name}, (err, result) => {
+    words.findOne({company: req.params.company_name}, (err, result) => {
         if (err) {
             console.log(err);
             res.json({"msg": "something error"});
@@ -16,7 +17,7 @@ router.get('/:company_name', function(req, res) {
 
 router.post('/', function(req, res) {
     var company_name = req.body.company_name;
-    var word = req.body.word;
+    var letter = req.body.word;
 
     words.findOne({company: company_name}, (err, result) => {
         if (err) {
@@ -24,10 +25,10 @@ router.post('/', function(req, res) {
             res.json({"msg": "something error"});
         }
 
-        if (result.length === 0) {
+        if (!result || result.length === 0) {
             var new_company_words = new words({
                 company: company_name,
-                powerfulWords: jancuk
+                powerfulWords: {word: letter}
             });
 
             new_company_words.save((err) => {
@@ -41,7 +42,7 @@ router.post('/', function(req, res) {
         else {
             //kalo udh ada, di tambah arraynya
             var new_powerful_words_array = result.powerfulWords;
-            new_powerful_words_array.push(word);
+            new_powerful_words_array.push({word: letter});
             
             result.update({powerfulWords: new_powerful_words_array}, (err) => {
                 if (err) 
@@ -51,6 +52,39 @@ router.post('/', function(req, res) {
                 } 
             });
         }
+    });
+});
+
+router.get('/:company_name/words', function(req, res) {
+    words.findOne({company: req.params.company_name}, (err, result) => {
+        if (err) {
+            console.log(err);
+            res.json({"msg": "something error"});
+        }
+
+        var words_not_visited = data.filter(data => data.is_visited === "0");
+        var words = result.powerfulWords;
+
+        var calculate = function(words, words_not_visited) {
+            console.log("wtf");
+          return new Promise((resolve,reject) => {
+             words_not_visited.forEach(function(part, idx, words_not_visited) {
+                words.forEach(function(part2, index, words) {
+                    if (words_not_visited[idx].text.split(' ').indexOf(words[index].word) > -1) 
+                        words[index].counter += 1;
+                });
+             });
+
+             resolve(words);
+          });
+        };
+
+        calculate(words, words_not_visited).then(result => {
+            console.log(result);
+
+            //after all, save
+            res.json({"msg": "wk"});
+        });
     });
 });
 
