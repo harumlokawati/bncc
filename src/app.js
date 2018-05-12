5efeb8b34f999dcbbf9e49213f3c0d318eb4f048
Taufan Mahaputra <taufanmhptr@gmail.com>
2018-05-12 20:05:39 +0700

    clean code

--- src/app.js
+++ src/app.js
@@ -1,39 +1,39 @@
-
-
 var express = require('express'),
-cors = require('cors'),
-db = require('mongoose'),
-app = express(),
-bodyParser = require('body-parser');
+	cors = require('cors'),
+	db = require('mongoose'),
+	bodyParser = require('body-parser'),
+	app = express(),
+	Twit = require('twit');
+
+var config = require("./config.json");
 
 // db.connect('http://localhost:27017/bncc', function() {
 // 	console.log('connected');
 // });
 
 app.use(cors());
-// app.use(bodyparser)
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function(req, res) {
-console.log('crot');
-res.send('hello words');
+	res.send('Webservice');
 });
-var Twit = require('twit');
+
 
 var T = new Twit({
-consumer_key:         'hSAHglnYD3IRqFLf2qAQvtBgT',
-consumer_secret:      'n4xErXSHAQUybp5SqRSNwtNSADWcdCVfdYIktkg3DWafSUM1Fr',
-access_token:         '212796362-KNX9xWRcYwWnAib7rFSfviM4nY5Z9kqzrElVQS9n',
-access_token_secret:  '07TdK7TPcrUMlLdG2eSgH3bZYBmDASZlaQDcWxMXMVMU6',
-timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+	consumer_key:         config.consumer_key,
+	consumer_secret:      config.consumer_secret,
+	access_token:         config.access_token,
+	access_token_secret:  config.access_token_secret,
+	timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 });
 
 var stream = T.stream('statuses/filter', {track: 'tokopedia'});
 
 stream.on('tweet', function(tweet) {
-console.log(tweet);
+	console.log(tweet);
 });
+
 app.listen(3000, function() {
-console.log('App Started');
+	console.log('App Started');
 });
